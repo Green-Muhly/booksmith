c5ea71bd004cc8efbca3d61f043ccaccd85b3194
souvenir718 <mongkey0@naver.com>
2021-08-21 12:53:11 +0900

    [0821] Login 페이지 UI 간단하게 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
+import { Login } from "./pages";
 
 const App = () => {
     return (
-        <Switch>
-            <Route exact={true} path="/">
-                <div>App</div>
-            </Route>
-        </Switch>
+        <>
+            <Switch>
+                <Route exact={true} path="/">
+                    <Link to="login">Login</Link>
+                </Route>
+                <Route path="/test">
+                    <div>Test</div>
+                </Route>
+                <Route path="/login" component={Login} />
+            </Switch>
+        </>
     );
 };
 
