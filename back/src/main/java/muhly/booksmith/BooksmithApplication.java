b08e83357e7f606e481968cddfeaa643f9e5f768
Hyunta <mohai2618@naver.com>
2021-08-24 15:03:27 +0900

    Integrate Repository

---
+++ back/src/main/java/muhly/booksmith/BooksmithApplication.java
@@ -0,0 +1,13 @@
+package muhly.booksmith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksmithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksmithApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/muhly/booksmith/BooksmithApplicationTests.java
@@ -0,0 +1,13 @@
+package muhly.booksmith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksmithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
