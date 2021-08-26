093a0938ac235fe28edfd3544543e92222eca66d
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-08-27 07:59:49 +0900

    [0827] 회원가입 레이아웃 구현

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { Link, Route, Switch } from "react-router-dom";
-import { Login } from "./pages";
+import { Login, SignUp } from "./pages";
 
 const App = () => {
-    return (
-        <>
-            <Switch>
-                <Route exact={true} path="/">
-                    <Link to="login">Login</Link>
-                </Route>
-                <Route path="/test">
-                    <div>Test</div>
-                </Route>
-                <Route path="/login" component={Login} />
-            </Switch>
-        </>
-    );
+  return (
+    <>
+      <Switch>
+        <Route exact={true} path="/">
+          <Link to="login">Login</Link>
+        </Route>
+        <Route path="/test">
+          <div>Test</div>
+        </Route>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </>
+  );
 };
 
 export default App;
