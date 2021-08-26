c85bd722ee32899e257c6054383ea48948a925c5
Hyunta <mohai2618@naver.com>
2021-08-26 15:56:31 +0900

    feat(BookBack) : Add Member domain

---
+++ back/src/main/java/muhly/booksmith/domain/member/Member.java
@@ -0,0 +1,32 @@
+package muhly.booksmith.domain.member;
+
+import lombok.Data;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotEmpty
+    private String loginId;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String password;
+    private Long phoneNumber;
+    private String email;
+
+    @Embedded
+    private Address address;
+
+
+}
