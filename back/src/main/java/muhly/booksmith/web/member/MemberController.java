6ce6f9f1b2902e16ff125e55d83e3ea23101c6d9
Hyunta <mohai2618@naver.com>
2021-08-28 22:59:18 +0900

    feat(Back) : Add MemberService, MemberController

--- back/src/main/java/muhly/booksmith/web/member/MemberController.java
+++ back/src/main/java/muhly/booksmith/web/member/MemberController.java
@@ -4,19 +4,32 @@ import lombok.RequiredArgsConstructor;
 import muhly.booksmith.domain.member.Member;
 import muhly.booksmith.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController {
 
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
     @GetMapping("/add")
-    public String addForm(@ModelAttribute("member")Member member) {
+    public String addForm(@ModelAttribute("member") Member member) {
         return "members/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Valid @ModelAttribute Member member, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "members/addMemberForm";
+        }
+        memberService.join(member);
+        return "redirect:/";
     }
 }
--- back/src/main/java/muhly/booksmith/web/member/MemberService.java
+++ back/src/main/java/muhly/booksmith/web/member/MemberService.java
@@ -1,13 +1,39 @@
 package muhly.booksmith.web.member;
 
+import lombok.RequiredArgsConstructor;
+import muhly.booksmith.domain.member.Member;
 import muhly.booksmith.domain.member.MemberRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+
 @Transactional
+@RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
     }
+
+    private void validateDuplicateMember(Member member) {
+        Optional<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() -> new NoSuchElementException("id가 존재하지 않습니다."));
+    }
+
 }
