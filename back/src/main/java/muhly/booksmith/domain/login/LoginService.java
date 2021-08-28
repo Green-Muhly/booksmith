ddf7e75ea51e1f9e7ac602f062cd974a179d2ea1
Hyunta <mohai2618@naver.com>
2021-08-28 23:04:51 +0900

    feat(Back) : Add LoginService

---
+++ back/src/main/java/muhly/booksmith/domain/login/LoginService.java
@@ -0,0 +1,20 @@
+package muhly.booksmith.domain.login;
+
+import lombok.RequiredArgsConstructor;
+import muhly.booksmith.domain.member.Member;
+import muhly.booksmith.domain.member.MemberRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+
+}
