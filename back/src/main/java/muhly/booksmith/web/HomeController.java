7600c0520a49f43e4fa6072f43e97bbb20b5647f
Hyunta <mohai2618@naver.com>
2021-08-29 15:50:30 +0900

    feat(Back) : Add HomeController

--- back/src/main/java/muhly/booksmith/web/HomeController.java
+++ back/src/main/java/muhly/booksmith/web/HomeController.java
@@ -1,15 +1,25 @@
 package muhly.booksmith.web;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import muhly.booksmith.domain.member.Member;
+import muhly.booksmith.web.argumentresolver.Login;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String home(@Login Member loginMember, Model model) {
+        if (loginMember == null) {
+            return "home";
+        }
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+
     }
 }
