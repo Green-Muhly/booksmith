64b9a7fb59888e5b098aa9bfeef55a51452a2742
Hyunta <mohai2618@naver.com>
2021-10-08 14:27:34 +0900

    feat(Back) : Add Item Domain

---
+++ back/src/main/java/muhly/booksmith/domain/item/Item.java
@@ -0,0 +1,26 @@
+package muhly.booksmith.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+
+    @ManyToMany
+    private List<Category> categories = new ArrayList<>();
+
+}
